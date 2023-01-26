80d6554b9c393856ceda93ca72a80abde9e602d3
luoqiming82 <lqmsums@gmail.com>
2023-01-27 00:35:04 +0800

    creat an obj to store messages

---
+++ script.js
@@ -0,0 +1,16 @@
+// the goal is to randomly pick 2 different workouts(each workout time randomly assigned, can be same or different) in a day. 
+// format: 'Monday', 'jogging 30minutes + tai chi 30 minutes'
+
+
+function workoutplaner(){
+    const combos = {
+
+        day: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        time:['30 minutes', '45 minutes', '60 minutes'],
+        workouts:['pushup and squat', 'jogging', 'sprinting', 'jumping jack', 'burpee', 'kungfu', 'katate', 'taekwondo', 'Tai Chi', 'yoga']
+    }
+
+    //pick day
+
+
+}
