7e01ddf0f323a944b335081099aee5b524d16177
luoqiming82 <qimingluo28@gmail.com>
2023-01-27 22:03:37 +0800

    modify code, add a parameter to function, make it more general

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-// the goal is to randomly pick 2 different workouts(each workout time randomly assigned, can be same or different) in a day. 
+// the goal is to randomly pick 2 different workouts(each workout time randomly assigned, can be same or different) in a randomly picked day.
+ 
 // format: 'Monday', 'jogging 30minutes ; tai chi 30 minutes'
 
 
-function workoutplaner(){
-    const combos = {
-        day: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-        time:['30 minutes', '45 minutes', '60 minutes'],
-        workouts:['pushup and squat', 'jogging', 'sprinting', 'jumping jack', 'burpee', 'kungfu', 'katate', 'taekwondo', 'Tai Chi', 'yoga']
-    }
+function workoutplaner(obj){
+   
 
     //pick a day
-    const dayArr = combos.day;
+    const dayArr = obj.day;
     let indexDay = Math.floor(Math.random()*dayArr.length); // generate a random dayArr index
     let pickDay = dayArr[indexDay]; // get a random day 
 
     // pick a time 
-    const timeArr = combos.time;
+    const timeArr = obj.time;
     let indexTime1 = Math.floor(Math.random()*timeArr.length); // generate a random timeArr index
     let indexTime2 = Math.floor(Math.random()*timeArr.length);
     let pickTime1 = timeArr[indexTime1]; // get a random time for workout 1
     let pickTime2 = timeArr[indexTime2];  // get a random time for workout 2
 
     //pick 2 different workouts
-    const workoutArr = combos.workouts;
+    const workoutArr = obj.workouts;
     let indexWorkout1 = Math.floor(Math.random()*workoutArr.length); //  generate a random workoutArr index
     let indexWorkout2 = Math.floor(Math.random()*workoutArr.length); // generate another random workoutArr index
     let pickWorkout1 = workoutArr[indexWorkout1]; // get a random workout
@@ -37,6 +34,18 @@ function workoutplaner(){
     return result;
 }
 
-let workoutForADay = workoutplaner();
 
-console.log(workoutForADay);
+const combos = {
+    day: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    time:['30 minutes', '45 minutes', '60 minutes'],
+    workouts:['pushup and squat', 'jogging', 'sprinting', 'jumping jack', 'burpee', 'kungfu', 'katate', 'taekwondo', 'Tai Chi', 'yoga']
+}
+
+let workoutForADay = workoutplaner(combos);
+console.log(workoutForADay, '\n');
+/*
+for(let i=0; i<10; i++){
+    let workoutForADay = workoutplaner(combos);
+    console.log(workoutForADay, '\n');
+}
+*/
